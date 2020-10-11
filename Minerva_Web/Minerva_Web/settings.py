dbd784dcce7c4cd1c989cbc5503db1f0575207ff
RaghavBhat02 <raghavbhat02@gmail.com>
2020-10-10 20:37:38 -0400

    staged initial run

--- Minerva_Web/Minerva_Web/settings.py
+++ Minerva_Web/Minerva_Web/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Minerva_Home'
+    'Minerva_Home',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
