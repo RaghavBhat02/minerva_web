ae7d262303b0405ec9148a49a052bd9a4522fdd5
trt2002 <tanaythakur@Tanays-MacBook-Pro.local>
2020-10-20 19:22:49 -0400

    views.py login stuff

--- Minerva_Web/Minerva_Home/urls.py
+++ Minerva_Web/Minerva_Home/urls.py
@@ -5,7 +5,7 @@ app_name = "Minerva_Home"
 urlpatterns = [
     path("", views.index, name="index"),
     #path("logged", views.logged, name = "logged" ),
-    #path("login", views.login, name = "login"),
-    #path("logout",views.logout, name = "logout")
+    path("login", views.login_view, name = "login"),
+    path("logout",views.logout_view, name = "logout")
 
 ]
