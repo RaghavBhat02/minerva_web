98df4906b85bb02c0b8b29c518e4bb90f9c01012
RaghavBhat02 <raghavbhat02@gmail.com>
2020-10-22 18:26:55 -0400

    html, css, added models, added background-image in static folder. Created navbar.

--- minerva_web/home/admin.py
+++ minerva_web/home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import image
 # Register your models here.
+class image_admin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+admin.site.register(image, image_admin)
--- minerva_web/home/models.py
+++ minerva_web/home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class image(models.Model):
+    image = models.ImageField(upload_to = 'home/images', blank = False)
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f"{self.name}"
