74c851640b37ca2e44fbe5e6698d336283dfd535
trt2002 <tanaythakur@Tanays-MacBook-Pro.local>
2020-10-24 19:55:48 -0400

    created login form for users

--- minerva_web/home/urls.py
+++ minerva_web/home/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "home"
 
 urlpatterns = [
-    path("",views.index,name="index")
+    path("",views.index,name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout")
 ]
--- minerva_web/home/views.py
+++ minerva_web/home/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
 from .models import image
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def index(request):
     return render(request,"home/index.html", {
         "images":image.objects.all()
     })
+
+
+
+def logout_view(request):
+     pass
+     # Pass is a simple way to tell python to do nothing.
+
+    # Additional imports we'll need:
+
+
+def login_view(request):
+    if request.method == "POST":
+        # Accessing username and password from form data
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        # Check if username and password are correct, returning User object if so
+        user = authenticate(request, username=username, password=password)
+
+        # If user object is returned, log in and route to index page:
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        # Otherwise, return login page again with new context
+        else:
+            return render(request, "home/login.html", {
+                "message": "Invalid Credentials"
+            })
+    return render(request, "home/login.html")
