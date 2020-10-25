602e12732ad6d039d0bb223d381ad6b723a3c89a
RaghavBhat02 <raghavbhat02@gmail.com>
2020-10-24 22:43:49 -0400

    pycaches

--- minerva_web/home/views.py
+++ minerva_web/home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, reverse
 from .models import image
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
+
 # Create your views here.
 
 def index(request):
