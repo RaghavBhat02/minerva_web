32867d4de5ba6900ef5b1b059b7b2c4457bfb1ef
RaghavBhat02 <raghavbhat02@gmail.com>
2020-11-06 17:46:14 -0500

    sign up system

--- minerva_web/home/views.py
+++ minerva_web/home/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, reverse
 from .models import image
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -38,3 +39,27 @@ def login_view(request):
                 "message": "Invalid Credentials"
             })
     return render(request, "home/login.html")
+
+def signup_view(request):
+    if request.method == "POST": #check if method is post
+        password = request.POST["password"] #check if both entered passwords match
+        password_confirm = request.POST["password-confirm"]
+        if not password == password_confirm:
+            return render(request, "home/signup.html",{
+            "message": "Passwords don't match!" #if they don't match say that they don't match
+            })
+        email = request.POST["email"] #check if email ends with umich.edu
+        if not email.endswith('@umich.edu'):
+            return render(request, "home/signup.html",{
+            "message": "Please use your UMich email! Your email should end with @umich.edu" #if they don't match say that they don't match
+            })
+        username = request.POST["username"]
+        first_name = request.POST["firstname"]
+        last_name = request.POST["lastname"]
+        user = User.objects.create_user(username, email, password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
+        return HttpResponse("USER CREATED!")
+
+    return render(request,"home/signup.html")
